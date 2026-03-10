8321410fe6f956421346310cb82864efbf21993e
pjd199 <89210268+pjd199@users.noreply.github.com>
2026-03-10 17:03:20 +0000

    Update pro-media-cropper.php

--- src/pro-media-cropper.php
+++ src/pro-media-cropper.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Pro Media Cropper
  * Description: Precision cropping tool with advanced crop options and stock image search function.
- * Version: 3.9.1
+ * Version: 3.9.2
  * Author: Pete Dibdin
  * GitHub Plugin URI: https://github.com/pjd199/pro-media-cropper
  * License: MIT
